4cfebffee70c7bdbe9458288de7312430b19fc35
Aneeshie <aneeshdas556@gmail.com>
2025-08-20 12:04:56 +0530

    fix: boot issue with i3

--- install.sh
+++ install.sh
@@ -14,6 +14,13 @@ error() {
   echo -e "${RED}[ERROR]${RESET} $1"
 }
 
+info "Installing base packages..."
+sudo pacman -Syu --noconfirm
+
+info "Installing Xorg (display server)..."
+sudo pacman -S --noconfirm xorg xorg-xinit
+
+
 info "Checking if i3 is installed..."
 if ! command -v i3 >/dev/null 2>&1; then
   info "Installing i3..."
@@ -22,8 +29,11 @@ else
   info "i3 is already installed."
 fi
 
+info "Setting up .xinitrc to start i3..."
+echo "exec i3" > "$HOME/.xinitrc"
+
 if ! command -v yay >/dev/null 2>&1; then
-  info "Installing yay..."
+  info "Installing yay (AUR helper)..."
   sudo pacman -S --noconfirm --needed git base-devel
   git clone https://aur.archlinux.org/yay.git /tmp/yay
   cd /tmp/yay && makepkg -si --noconfirm
@@ -33,7 +43,7 @@ else
 fi
 
 info "Installing ghostty, polybar, rofi, picom, neovim, tmux, zsh..."
-yay -S --noconfirm ghostty-git polybar rofi picom neovim tmux zsh
+yay -S --noconfirm ghostty polybar rofi picom neovim tmux zsh xclip
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   info "Installing oh-my-zsh..."
@@ -44,8 +54,8 @@ fi
 
 DOTFILES_REPO="https://github.com/Aneeshie/arch-dotfiles.git"
 CLONE_PATH="$HOME/.config"
-
 BACKUP_PATH="$HOME/.config.backup.$(date +%s)"
+
 if [ -d "$CLONE_PATH" ]; then
   info "Backing up existing .config to $BACKUP_PATH"
   mv "$CLONE_PATH" "$BACKUP_PATH"
@@ -70,5 +80,4 @@ else
   error ".tmux.conf not found in $CLONE_PATH/"
 fi
 
-info "Done! Enjoyyy "
-
+info "Done! Enjoyyyyy"
